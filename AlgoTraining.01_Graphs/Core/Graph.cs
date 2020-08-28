99fbd0f67ccd210c484c262fb2ed3a9c443a4a80
Momchil Momchilov <momchil@kodar.net>
2020-08-28 11:35:36 +0300

    Fixed indentation.

--- AlgoTraining.01_Graphs/Core/Graph.cs
+++ AlgoTraining.01_Graphs/Core/Graph.cs
@@ -5,65 +5,65 @@ using System.Text;
 
 namespace AlgoTraining._01_Graphs.Core
 {
-	public class Graph<TVertex>
-	{
-		private readonly Dictionary<TVertex, List<TVertex>> _map;
+    public class Graph<TVertex>
+    {
+        private readonly Dictionary<TVertex, List<TVertex>> _map;
 
-		public Graph()
-		{
-			_map = new Dictionary<TVertex, List<TVertex>>();
-		}
+        public Graph()
+        {
+            _map = new Dictionary<TVertex, List<TVertex>>();
+        }
 
-		public List<TVertex> this[TVertex vertex]
-		{
-			get
-			{
-				List<TVertex> edges;
-				_map.TryGetValue(vertex, out edges);
-
-				return edges;
-			}
-		}
-
-		public List<TVertex> Vertices
-		{
-			get
-			{
-				return _map.Keys?.ToList();
-			}
-		}
-
-		public void Connect(TVertex src, TVertex dest)
-		{
-			if (!_map.ContainsKey(src))
-				_map.Add(src, new List<TVertex>());
-
-			if (!_map.ContainsKey(dest))
-				_map.Add(dest, new List<TVertex>());
-
-			this[src].Add(dest);
-		}
-
-		public void Disconnect(TVertex src, TVertex dest)
-		{
-			if (!_map.ContainsKey(src)
-				|| !_map.ContainsKey(dest))
+        public List<TVertex> this[TVertex vertex]
+        {
+            get
             {
-				throw new ArgumentOutOfRangeException();
+                List<TVertex> edges;
+                _map.TryGetValue(vertex, out edges);
+
+                return edges;
+            }
+        }
+
+        public List<TVertex> Vertices
+        {
+            get
+            {
+                return _map.Keys?.ToList();
+            }
+        }
+
+        public void Connect(TVertex src, TVertex dest)
+        {
+            if (!_map.ContainsKey(src))
+                _map.Add(src, new List<TVertex>());
+
+            if (!_map.ContainsKey(dest))
+                _map.Add(dest, new List<TVertex>());
+
+            this[src].Add(dest);
+        }
+
+        public void Disconnect(TVertex src, TVertex dest)
+        {
+            if (!_map.ContainsKey(src)
+                || !_map.ContainsKey(dest))
+            {
+                throw new ArgumentOutOfRangeException();
             }
 
-			List<TVertex> adjacentVertices = this[src];
+            List<TVertex> adjacentVertices = this[src];
 
             if (adjacentVertices.Count <= 1)
             {
-				_map.Remove(src);
-				return;
+                _map.Remove(src);
+                return;
             }
 
-			// Do we want to throw an exception here?
-			TVertex vertexToRemove = adjacentVertices.First(v => v.Equals(dest));
+            // Do we want to throw an exception here?
+            TVertex vertexToRemove = adjacentVertices.First(v => v.Equals(dest));
 
-			adjacentVertices.Remove(vertexToRemove);
+            adjacentVertices.Remove(vertexToRemove);
         }
-	}
+    }
 }
